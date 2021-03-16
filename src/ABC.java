3008b36f28c3421c6e77eb243a570bfc8022e3be
Paulgazza2008 <paulkafumbe@gmail.com>
2021-03-16 19:50:50 +0300

    init commit

---
+++ src/ABC.java
@@ -0,0 +1,18 @@
+import java.util.regex.Pattern;
+
+public class ABC {
+
+    public static void main(String[] args) {
+
+        String input = "2[xyz]4[xy]z";
+        String output = "xyzxyzxyzxyxyxyxyz";
+
+        Pattern pattern = Pattern.compile("[\\[ ,.!?\\]]");
+
+        String []S = pattern.split(output);
+
+        for(String word:S)
+
+            System.out.println(word);
+    }
+}
